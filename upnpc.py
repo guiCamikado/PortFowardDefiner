d189d2360a9e516d59da8181a298fe288caed5ea
guiCamikado <guilherme.camikado@gmail.com>
2025-12-15 17:24:42 -0300

    .

--- upnpc.py
+++ upnpc.py
@@ -1,15 +1,32 @@
 import miniupnpc
+help(miniupnpc.UPnP)
 
-upnp = miniupnpc.UPnP()
-upnp.discoverdelay = 200
+class UpnpcTools:
+    upnp = miniupnpc.UPnP()
 
-try:
-    n_devices = upnp.discover()
-    print(f"Dispositivos encontrados: {n_devices}")
-except Exception as e:
-    print("Aviso do UPnP:", e)
 
-upnp.selectigd()
+# WIP não da para testar isso aqui no momento! testar em casa
+    @classmethod
+    def upnpcDiscover(cls):
+        cls.upnp.discoverdelay = 200
 
-print("IP externo:", upnp.externalipaddress())
-print("IP interno:", upnp.lanaddr)
+        try:
+            n_devices = cls.upnp.discover()
+            print(f"Dispositivos encontrados: {n_devices}")
+        except Exception as e:
+            print("Aviso do UPnP:", e)
+
+        cls.upnp.selectigd() #Internet Gateway Device
+
+        print("IP externo:", cls.upnp.externalipaddress())
+        print("IP interno:", cls.upnp.lanaddr)
+
+    @classmethod
+    def getIp(cls):
+        wan_ip = cls.upnp.externalipaddress()  # pega o IP externo correto
+        print(wan_ip)
+        return wan_ip
+        
+
+
+UpnpcTools.getIp()
