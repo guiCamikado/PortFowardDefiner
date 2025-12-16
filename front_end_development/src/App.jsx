4e069768ca0cdd1cc5f99161282692f13cf1373d
guiCamikado <guilherme.camikado@gmail.com>
2025-12-16 17:07:49 -0300

    organizando o amanhã

--- front_end_development/src/App.jsx
+++ front_end_development/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  // WIP Coloquei aqui porque sei que amanhã eventualmente eu vou mexer... Seguinte faz uma interface com tailwind e usa aquele negócio do black & white você precisa de uma entrada de portas, uma que armazena as portas abertas, um botão para deletar portas abertas, outro para editar que vai ser um delete seguido de um create que pega os parâmetros um local que recebe o seu IP. lembrando que não da para abrir portas de outros IPs então não deve ser possível de editar o mesmo.
+
+  // Por hora melhor não pensar nisso por hora mas vai ser necessário um banco de dados sqlite aqui
   return (
     <>
       <div>
@@ -29,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
